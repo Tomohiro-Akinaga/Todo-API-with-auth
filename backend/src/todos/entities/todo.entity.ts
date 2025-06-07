5b201fe41df44fd159b0e6064f977e78463d5a25
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-06-07 22:06:41 +0800

    feat: TodoEntityを作成

--- backend/src/todos/entities/todo.entity.ts
+++ backend/src/todos/entities/todo.entity.ts
@@ -1,1 +1,19 @@
-export class Todo {}
+import { Todo } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class TodoEntity implements Todo {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  text: string;
+
+  @ApiProperty()
+  completed: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -10,7 +10,8 @@ import {
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { TodoEntity } from './entities/todo.entity';
 
 @Controller('todos')
 @ApiTags('todos')
@@ -18,26 +19,31 @@ export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: TodoEntity })
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todosService.create(createTodoDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: TodoEntity, isArray: true })
   findAll() {
     return this.todosService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: TodoEntity })
   findOne(@Param('id') id: string) {
     return this.todosService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: TodoEntity })
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(id, updateTodoDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: TodoEntity })
   remove(@Param('id') id: string) {
     return this.todosService.remove(id);
   }
